fc5b2d584fefa6322f1fe0dcfcdab5d6cc138ff4
josh <113545002+113831josh@users.noreply.github.com>
2023-02-08 13:58:28 -0500

    Add files via upload

---
+++ pygame/rgrg.py
@@ -0,0 +1,14 @@
+natureblank = Actor('character')
+natureblank.pos = 100, 150
+
+WIDTH = 500
+HEIGHT = 300
+
+def draw():
+    screen.fill((100, 0 ,0))
+    natureblank.draw()
+
+def update():
+    natureblank.left = natureblank.left + 2
+    if natureblank.left > WIDTH:
+        natureblank.right = 0
